f5c850c56d75a05981c26e7daef7f3b07c62d05f
Raitis Stengrevics <dagrevis@gmail.com>
2016-06-22 20:14:29 +0300

    Improve looks for forms, refactor stylesheeets a bit

--- app/frontend/payment-form.jsx
+++ app/frontend/payment-form.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "payment-form.scss"
+
 class DeleteLink extends React.Component {
 
     onClick = (ev) => {
@@ -35,22 +37,66 @@ class PaymentForm extends React.Component {
 
         return <div className="payment-form">
             <form>
-                <label htmlFor="price">Price</label>
-                <input
-                    id="price"
-                    placeholder="EUR"
-                    value={payment.price}
-                    onChange={(ev) => this.onChange(ev, "price")}
-                />
+                <div className="row">
+                    <div>
+                        <label className="label" htmlFor="price">
+                            Price
+                        </label>
+                    </div>
+                    <input
+                        id="price"
+                        type="text"
+                        placeholder="EUR"
+                        value={payment.price}
+                        onChange={(ev) => this.onChange(ev, "price")}
+                    />
+                </div>
 
-                <label htmlFor="name">Name</label>
-                <input
-                    id="name"
-                    value={payment.name}
-                    onChange={(ev) => this.onChange(ev, "name")}
-                />
+                <div className="row">
+                    <div>
+                        <label className="label" htmlFor="name">
+                            Name
+                        </label>
+                    </div>
+                    <input
+                        type="text"
+                        id="name"
+                        value={payment.name}
+                        onChange={(ev) => this.onChange(ev, "name")}
+                    />
+                </div>
 
-                <button onClick={this.onSubmit}>Save</button>
+                {/*
+                <div className="row">
+                    <div>
+                        <label className="label" htmlFor="category">
+                            Category
+                        </label>
+                    </div>
+                    <div className="select">
+                        <div />
+                        <select id="category">
+                            <option>Food</option>
+                            <option>Junk</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="row transaction-type">
+                    <div className="radio">
+                        <input name="rcp_gateway" value="paypal" id="paypal" defaultChecked="true" type="radio" />
+                        <label htmlFor="paypal">Withdrawal</label>
+                    </div>
+                    <div className="radio">
+                        <input name="rcp_gateway" value="stripe" id="stripe" type="radio" />
+                        <label htmlFor="stripe">Deposit</label>
+                    </div>
+                </div>
+                */}
+
+                <div className="row submit-button">
+                    <button onClick={this.onSubmit}>Save</button>
+                </div>
             </form>
         </div>
     }
